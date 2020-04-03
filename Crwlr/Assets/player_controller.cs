4afed946e4629bcc1ef98385fa40ad729bab111e
Matthiiijas <matthi1612@gmail.com>
2020-04-03 20:51:53 +0200

    keine Ahnung

--- Crwlr/Assets/player_controller.cs
+++ Crwlr/Assets/player_controller.cs
@@ -4,31 +4,61 @@ using UnityEngine;
 
 public class player_controller : MonoBehaviour
 {
+    Rigidbody2D rb;
     Animator anim;
+    SpriteRenderer sr;
     InputMaster controls;
     Vector2 movement;
 
     public float speed;
-    string orientation;
+    public string orientation;
 
-    Rigidbody2D rb;
 
     void Awake () {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
 
         controls = new InputMaster();
         controls.player.move.performed += ctx => movement = ctx.ReadValue<Vector2>();
     }
 
     void FixedUpdate () {
+        //Movement
         rb.velocity = movement*speed;
-        if(rb.velocity.y < 0) orientation = "down";
-        if(rb.velocity.y > 0) orientation = "up";
-        if(orientation == "up" && rb.velocity.magnitude > 0) anim.Play("walk_back");
-        if(orientation == "up" && rb.velocity.magnitude == 0) anim.Play("idle_back");
-        if(orientation == "down" && rb.velocity.magnitude > 0) anim.Play("walk_front");
-        if(orientation == "down" && rb.velocity.magnitude == 0) anim.Play("idle_front");
+
+        //Get direction
+        float angle = Vector2.SignedAngle(movement,new Vector2(1,0));
+        Debug.Log(angle);
+        if(movement != Vector2.zero) {
+            if(angle < -135) orientation = "left";
+            else if(angle <= -45) orientation = "up";
+            else if(angle < 45) orientation = "right";
+            else if(angle <= 135) orientation = "down";
+            else orientation = "left";
+        }
+
+        //Animation
+        if(orientation == "left") {
+            sr.flipX = true;
+            if(rb.velocity.magnitude > 0) anim.Play("idle_side"); // walk_side
+            else anim.Play("idle_side");
+            }
+        if(orientation == "right") {
+            sr.flipX = false;
+            if(rb.velocity.magnitude > 0) anim.Play("idle_side"); //walk_side
+            else anim.Play("idle_side");
+            }
+        if(orientation == "up") {
+            sr.flipX = false;
+            if(rb.velocity.magnitude > 0) anim.Play("walk_back");
+            else anim.Play("idle_back");
+            }
+        if(orientation == "down") {
+            sr.flipX = false;
+            if(rb.velocity.magnitude > 0) anim.Play("walk_front");
+            else anim.Play("idle_front");
+            }
     }
 
     void OnEnable () {
