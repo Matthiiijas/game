adadda33f3d354f3a8f6e05926c7be16748fa523
Matthiiijas <matthi1612@gmail.com>
2020-04-03 15:04:38 +0200

    new input system

--- Crwlr/Assets/player_controller.cs
+++ Crwlr/Assets/player_controller.cs
@@ -1,41 +1,33 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class player_controller : MonoBehaviour
 {
-    Rigidbody2D rb2d;
+    InputMaster controls;
+    Vector2 movement;
 
     public float speed;
-    public Vector2 movement,movement_raw;
-    public Vector2 orientation_raw;
-    public string orientation;
 
-    void Start()
-    {
-        rb2d = GetComponent<Rigidbody2D>();
+    Rigidbody2D rb;
+
+    void Awake () {
+        rb = GetComponent<Rigidbody2D>();
+
+        controls = new InputMaster();
+        controls.player.move.performed += ctx => movement = ctx.ReadValue<Vector2>();
     }
 
-    void FixedUpdate()
-    {
-        //Movement
-        movement_raw = new Vector2(Input.GetAxis("MoveX")*speed,Input.GetAxis("MoveY")*speed);
-        movement = Vector2.ClampMagnitude(movement_raw, 10);
-        rb2d.velocity = movement;
+    void FixedUpdate () {
+        Debug.Log(movement.magnitude);
+        rb.velocity = movement*speed;
+    }
 
+    void OnEnable () {
+        controls.Enable();
+    }
 
-        //Orientation
-        orientation_raw = new Vector2(Input.GetAxis("LookX"),Input.GetAxis("LookY"));
-        float angle = Vector2.SignedAngle(orientation_raw,new Vector2(1,0));
-        if(angle < -135) orientation = "left";
-        else if(angle < -45) orientation = "down";
-        else if(angle < 45) orientation = "right";
-        else if(angle < 135) orientation = "up";
-        else orientation = "left";
-
-        /*switch(orientation) {
-            case "left": Animator.Play("idle");
-            ...
-        }*/
+    void OnDisable () {
+        controls.Disable();
     }
 }
